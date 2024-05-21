3d0bc4b2016f3910e221a6e624155398872958f7
Thomazrlima <thomazlima211@gmail.com>
2024-05-21 18:27:38 -0300

    FIFO Funcionando

--- Implementation/main.c
+++ Implementation/main.c
@@ -8,7 +8,7 @@
 
 typedef struct {
     int num_pagina;
-} PageTableEntry;
+} PageTable;
 
 typedef struct {
     int num_frame;
@@ -20,9 +20,9 @@ typedef struct {
 } Frame;
 
 Frame memoria[FRAME_TAMANHO];
-PageTableEntry page_table[FRAME_TAMANHO];
+PageTable page_table[FRAME_TAMANHO];
 
-// Leitura do Addresses
+// Leitura dos Addresses
 int* ler_enderecos(const char* caminho, int* tamanho);
 char** int_para_binario(int* enderecos, int tamanho);
 char** extrair_offset(char** enderecos_binarios, int tamanho);
@@ -31,19 +31,22 @@ char** extrair_pagina(char** enderecos_binarios, int tamanho);
 // Memoria
 void iniciar_memoria();
 void iniciar_page_table();
-void atualizar_frame(int indice_frame, int num_pagina, int tempo_atual);
+void atualizar_frame(int num_frame, int num_pagina);
 void atualizar_page_table(int num_pagina, int num_frame);
 
-//Backing_Storage
+// Backing_Storage
 void ler_backing_store(FILE *backing_store, int num_pagina, char *buffer);
-void acessar_memoria(FILE *backing_store, int num_pagina, int offset, int tempo_atual);
+void acessar_memoria(FILE *backing_store, int num_pagina, int offset);
+
+// FIFO Queue
+void adicionar_fifo(Frame *memoria, int frame);
+int remover_fifo(Frame *memoria);
 
 int main() {
     int tamanho;
     int* enderecos = ler_enderecos("D:/PENTES/Pessoal/Virtual_Memory_Manager/Implementation/addresses.txt", &tamanho);
     char** enderecos_binarios = int_para_binario(enderecos, tamanho);
 
-    // Inicializa a memória e a tabela de páginas
     iniciar_memoria();
     iniciar_page_table();
 
@@ -76,7 +79,7 @@ int main() {
         for (int i = 0; i < tamanho; i++) {
             int num_pagina = (int)strtol(pagina[i], NULL, 2);
             int offset_val = (int)strtol(offset[i], NULL, 2);
-            acessar_memoria(backing_store, num_pagina, offset_val, i);
+            acessar_memoria(backing_store, num_pagina, offset_val);
         }
 
         fclose(backing_store);
@@ -96,7 +99,7 @@ int main() {
     return 0;
 }
 
-// Leitura do addresses
+// Leitura dos Addresses
 int* ler_enderecos(const char* caminho, int* tamanho) {
     FILE *file = fopen(caminho, "r");
     if (!file) {
@@ -104,26 +107,23 @@ int* ler_enderecos(const char* caminho, int* tamanho) {
     }
 
     int* enderecos = NULL;
-    int contador = 0;
+    int cont = 0;
     int capacidade = 10;
 
     enderecos = (int*)malloc(capacidade * sizeof(int));
 
-    while (fscanf(file, "%d", &enderecos[contador]) == 1) {
-        contador++;
-        if (contador >= capacidade) {
+    while (fscanf(file, "%d", &enderecos[cont]) == 1) {
+        cont++;
+        if (cont >= capacidade) {
             capacidade *= 2;
-            int* novos_enderecos = (int*)realloc(enderecos, capacidade * sizeof(int));
-            enderecos = novos_enderecos;
+            enderecos = (int*)realloc(enderecos, capacidade * sizeof(int));
         }
     }
 
     fclose(file);
 
-    int* enderecos_finais = (int*)realloc(enderecos, contador * sizeof(int));
-
-    *tamanho = contador;
-    return enderecos_finais;
+    *tamanho = cont;
+    return enderecos;
 }
 
 char** int_para_binario(int* enderecos, int tamanho) {
@@ -131,30 +131,12 @@ char** int_para_binario(int* enderecos, int tamanho) {
 
     for (int i = 0; i < tamanho; i++) {
         int numero = enderecos[i];
-        int tamanho_binario = 0;
-        int temp = numero;
-
-        while (temp > 0) {
-            tamanho_binario++;
-            temp >>= 1;
-        }
-
-        int zero = 16 - tamanho_binario;
-        if (zero < 0) {
-            zero = 0;
-        }
-
-        enderecos_binarios[i] = (char*)malloc((16 + 1) * sizeof(char));
-        enderecos_binarios[i][16] = '\0';
-
-        for (int j = 0; j < zero; j++) {
-            enderecos_binarios[i][j] = '0';
-        }
-
-        for (int j = 15; j >= zero; j--) {
+        enderecos_binarios[i] = (char*)malloc(17 * sizeof(char));
+        for (int j = 15; j >= 0; j--) {
             enderecos_binarios[i][j] = (numero & 1) + '0';
             numero >>= 1;
         }
+        enderecos_binarios[i][16] = '\0';
     }
 
     return enderecos_binarios;
@@ -164,21 +146,8 @@ char** extrair_offset(char** enderecos_binarios, int tamanho) {
     char** offset = (char**)malloc(tamanho * sizeof(char*));
 
     for (int i = 0; i < tamanho; i++) {
-        int len = strlen(enderecos_binarios[i]);
         offset[i] = (char*)malloc(9 * sizeof(char));
-
-        if (len > 8) {
-            for (int j = 0; j < 8; j++) {
-                offset[i][j] = enderecos_binarios[i][len - 8 + j];
-            }
-        } else {
-            for (int j = 0; j < len; j++) {
-                offset[i][j] = enderecos_binarios[i][j];
-            }
-            for (int j = len; j < 8; j++) {
-                offset[i][j] = '0';
-            }
-        }
+        strncpy(offset[i], enderecos_binarios[i] + 8, 8);
         offset[i][8] = '\0';
     }
 
@@ -190,7 +159,7 @@ char** extrair_pagina(char** enderecos_binarios, int tamanho) {
 
     for (int i = 0; i < tamanho; i++) {
         pagina[i] = (char*)malloc(9 * sizeof(char));
-        strncpy(pagina[i], enderecos_binarios[i] + 0, 8);
+        strncpy(pagina[i], enderecos_binarios[i], 8);
         pagina[i][8] = '\0';
     }
 
@@ -214,43 +183,42 @@ void iniciar_page_table() {
     }
 }
 
-void atualizar_frame(int indice_frame, int num_pagina, int tempo_atual) {
-    memoria[indice_frame].num_pagina = num_pagina;
-    memoria[indice_frame].ocupado = 1;
-    memoria[indice_frame].ultimo_acesso = tempo_atual;
-    memoria[indice_frame].tempo = tempo_atual;
-    atualizar_page_table(num_pagina, indice_frame);
+void atualizar_frame(int num_frame, int num_pagina) {
+    memoria[num_frame].num_pagina = num_pagina;
+    memoria[num_frame].ocupado = 1;
+    memoria[num_frame].ultimo_acesso++;
+    memoria[num_frame].tempo++;
+    atualizar_page_table(num_pagina, num_frame);
 }
 
 void atualizar_page_table(int num_pagina, int num_frame) {
-    page_table[num_frame].num_pagina = num_pagina;
+    page_table[num_pagina].num_pagina = num_frame;
 }
 
-//Backing_Storage
+// Backing_Storage
 void ler_backing_store(FILE *backing_store, int num_pagina, char *buffer) {
     int offset = num_pagina * PAGE_SIZE;
     fseek(backing_store, offset, SEEK_SET);
     fread(buffer, sizeof(char), PAGE_SIZE, backing_store);
 }
 
-void acessar_memoria(FILE *backing_store, int num_pagina, int offset, int tempo_atual) {
+void acessar_memoria(FILE *backing_store, int num_pagina, int offset) {
     int frame_encontrado = -1;
     for (int i = 0; i < FRAME_TAMANHO; i++) {
-        if (page_table[i].num_pagina == num_pagina) {
+        if (memoria[i].ocupado && memoria[i].num_pagina == num_pagina) {
             frame_encontrado = i;
             break;
         }
     }
-
     if (frame_encontrado != -1) {
         printf("Pagina %d encontrada no frame %d\n", num_pagina, frame_encontrado);
-        atualizar_frame(frame_encontrado, num_pagina, tempo_atual);
+        memoria[frame_encontrado].ultimo_acesso++;
     } else {
-        printf("Pagina %d nao encontrada na memoria, buscando no BACKING STORE\n", num_pagina);
+        printf("Pagina %d nao encontrada na memoria\n", num_pagina);
 
         int frame_vazio = -1;
         for (int i = 0; i < FRAME_TAMANHO; i++) {
-            if (memoria[i].ocupado == 0) {
+            if (!memoria[i].ocupado) {
                 frame_vazio = i;
                 break;
             }
@@ -258,14 +226,61 @@ void acessar_memoria(FILE *backing_store, int num_pagina, int offset, int tempo_
 
         if (frame_vazio != -1) {
             printf("Frame vazio encontrado na memoria (frame %d)\n", frame_vazio);
-            char buffer[PAGE_SIZE];
-            ler_backing_store(backing_store, num_pagina, buffer);
-            memcpy(memoria[frame_vazio].dados, buffer, PAGE_SIZE);
-
-            atualizar_frame(frame_vazio, num_pagina, tempo_atual);
+            ler_backing_store(backing_store, num_pagina, memoria[frame_vazio].dados);
+            atualizar_frame(frame_vazio, num_pagina);
         } else {
-            printf("Realizando substituicao\n");
-            // Implementação da substituição de página
+            int frame_substituir = remover_fifo(memoria);
+            printf("Substituindo frame %d\n", frame_substituir);
+            ler_backing_store(backing_store, num_pagina, memoria[frame_substituir].dados);
+            atualizar_frame(frame_substituir, num_pagina);
         }
     }
+}
+
+// FIFO Queue
+void adicionar_fifo(Frame *memoria, int frame) {
+    for (int i = 0; i < FRAME_TAMANHO; i++) {
+        if (memoria[i].num_frame == frame) {
+            return;
+        }
+    }
+
+    int frame_vazio = -1;
+    for (int i = 0; i < FRAME_TAMANHO; i++) {
+        if (!memoria[i].ocupado) {
+            frame_vazio = i;
+            break;
+        }
+    }
+
+    if (frame_vazio != -1) {
+        memoria[frame_vazio].num_frame = frame;
+        memoria[frame_vazio].ocupado = 1;
+        memoria[frame_vazio].ultimo_acesso++;
+        memoria[frame_vazio].tempo++;
+    } else {
+        int frame_substituir = remover_fifo(memoria);
+        memoria[frame_substituir].num_frame = frame;
+        memoria[frame_substituir].ocupado = 1;
+        memoria[frame_substituir].ultimo_acesso++;
+        memoria[frame_substituir].tempo++;
+    }
+}
+
+int remover_fifo(Frame *memoria) {
+    int frame_substituir = -1;
+    int menor_tempo = memoria[0].tempo;
+
+    for (int i = 1; i < FRAME_TAMANHO; i++) {
+        if (memoria[i].tempo < menor_tempo) {
+            menor_tempo = memoria[i].tempo;
+            frame_substituir = i;
+        }
+    }
+
+    if (frame_substituir == -1) {
+        frame_substituir = 0;
+    }
+
+    return frame_substituir;
 }
