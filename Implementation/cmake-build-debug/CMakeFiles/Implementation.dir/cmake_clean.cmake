7b61bf2ca3924c5ef137e3cee09ab2993ba57dd3
Thomazrlima <thomazlima211@gmail.com>
2024-05-22 19:17:05 -0300

    Tudo ok, menos a TLB

---
+++ Implementation/cmake-build-debug/CMakeFiles/Implementation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Implementation.dir/main.c.obj"
+  "CMakeFiles/Implementation.dir/main.c.obj.d"
+  "Implementation.exe"
+  "Implementation.exe.manifest"
+  "Implementation.pdb"
+  "libImplementation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
